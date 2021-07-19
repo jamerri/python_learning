e6f4130fa702e7ef5b47dd0e94e229fce7e2681e
jamerri <jamerri@163.com>
2021-07-20 01:47:51 +0800

    完成了第一章的内容

---
+++ chapter-one/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/7/20 1:22
+# @Author : Jamerri
+# @File : __init__.py.py
---
+++ chapter-one/summary.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/7/20 1:24
+# @Author : Jamerri
+# @File : summary.py
+
+
+""" 第一章主要是安装python"""
+
+'本项目均采用Pycharm进行代码编写'
