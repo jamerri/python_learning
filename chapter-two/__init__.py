d90e5473e23f8f1254db4d745b7ad54df99507a3
jamerri <jamerri@163.com>
2021-07-23 10:48:12 +0800

    完成了第一章的内容

---
+++ chapter-two/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/7/23 0:13
+# @Author : Jamerri
+# @File : __init__.py.py
---
+++ chapter-two/multi_simple_message.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/7/23 0:40
+# @Author : Jamerri
+# @File : multi_simple_message.py
+
+message = "I am a handsome man！"
+print(message)
+
+message = "I like sleeping! "
+print(message)
